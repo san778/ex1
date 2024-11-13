6d49809a565929c2f6dda1098b5cf8a714f7976d
san778 <4b3g0067@stust.edu.tw>
2024-11-13 22:45:38 +0800

    sadf

--- ex1/ex1.cpp
+++ ex1/ex1.cpp
@@ -10,10 +10,26 @@ int main()
         cout << "請輸入有效月份: " << endl;
     }
     else {
-        if (month >= 3 && month <= 5) result = "春季";
-        else if (month >= 6 && month <= 8) result = "夏季";
-        else if (month >= 9 && month <= 11) result = "秋季";
-        else result = "冬季";
-        cout << month << "月是:" << result << endl;
+        switch (month) {
+        case 3:
+        case 4:
+        case 5:
+            result = "春季";
+            break;
+        case 6:
+        case 7:
+        case 8:
+            result = "夏季";
+            break;
+        case 9:
+        case 10:
+        case 11:
+            result = "秋季";
+            break;
+        default:
+            result = "冬季";
+            break;
+        }
+        cout << month << "月是" << result << endl;
     }
 }
